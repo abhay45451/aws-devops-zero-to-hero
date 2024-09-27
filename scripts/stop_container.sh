2009825a4733c600068220168df2318c48ddf83f
Abhay Bavistale <122510007+abhay45451@users.noreply.github.com>
2024-09-27 14:07:43 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,5 +2,9 @@
 set -e
 
 # Stop the running container (if any)
-$containerID= docker ps | awk -f "" '{print $1}'
-docker rm -f $containerID
+containerID=$(docker ps -q)  # Get the ID of the running container
+if [ -n "$containerID" ]; then  # Check if containerID is not empty
+    docker rm -f $containerID  # Force remove the container
+else
+    echo "No running container to stop."
+fi
