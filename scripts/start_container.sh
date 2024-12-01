08d51254cbd58d640070bffe5c9d435cfd7d7c0d
Abhay Bavistale <122510007+abhay45451@users.noreply.github.com>
2024-12-01 17:10:49 +0530

    added

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhay45/simple-python-app
+docker pull abhay45/sample-python-app
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 abhay45/sample-python-app
